ccf33b54f6dfd51f10ac2cc5c911d32daccd71cd
Sahil-Shadwal <sahilshadwal@gmail.com>
2024-11-23 14:58:08 +0530

    token manager app initialized

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+// src/App.tsx
+import { FC } from "react";
+import "./App.css";
+
+const App: FC = () => {
+  return (
+    <div className="App">
+      <h1>SPL Token Manager</h1>
+    </div>
+  );
+};
+
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+// vite.config.ts
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
+
+export default defineConfig({
+  plugins: [
+    react(),
+    nodePolyfills(), // Required for Solana web3.js
+  ],
+});
